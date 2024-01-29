3a87e1a805c1aa83ea214595abc6ffc9cea0a01f
Luis Yael Mendez <luisyaelms@gmail.com>
2024-01-29 03:53:55 -0600

    Iniciando app
    Se inicia la app en ionic

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
   constructor() {}
+
+  ngOnInit(): void {
+    //Definir variables
+    let numero=10;
+    debugger;
+    var nombre = "Yael";
+
+    const dato = 10;
+
+    console.log("Hola mundo");
+  }
 
 }
